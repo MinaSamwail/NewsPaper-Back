353e106a87788b75d8e36bc5b887d8f5ddf3eb2f
Lucas Souleyrasse <lucas.souleyrasse@gmail.com>
2021-01-13 15:51:28 +0100

    server side prepared

---
+++ server/app.js
@@ -0,0 +1,62 @@
+require("dotenv").config(); //connect .env file
+require("./config/dbConnection"); //connection to the database
+
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo")(session);
+const _DEV_MODE = false;
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, // White list of clients, allowing specific domains to communicate with the api.
+    credentials: true, //Allow client to send cookies.
+  })
+);
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  session({
+    store: new MongoStore({ mongooseConnection: mongoose.connection }), //Persist the session in the database
+    saveUninitialized: true,
+    resave: true,
+    secret: process.env.SESSION_SECRET,
+  })
+);
+
+if (_DEV_MODE) {
+  const User = require("./models/User");
+
+  app.use((req, res, next) => {
+    User.findOne({}) //Get a user from the DB (doesn't matter which)
+      .then((userDocument) => {
+        req.session.currentUser = userDocument._id; // Set that user as the loggedin user by puutin him the session.
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        // .finaly() will be called no matter if the promise failed or succeeded so we can safely call our next function here.
+        next();
+      });
+  });
+}
+// Router
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+module.exports = app;
---
+++ server/config/dbConnection.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((self) => {
+    console.log(`Connected to ${self.connection.name}`);
+  })
+  .catch((err) => {
+    console.log(`Àn error occured while connection to the database`);
+  });
